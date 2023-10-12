6ba1fba32affc41e90bb3165e2405c813c2adfa1
ohseungmin <ohmin2886@gmail.com>
2023-10-12 11:03:29 +0900

    재수정

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req,res) => {
 });
 
 server.listen(3500, (err) => {
-  if(err) {
+  if(!err) {
     console.error("서버 재구동 필요")
   } else {
     // 팁 콘솔에 링크를 만들어두면 링크를 클릭하기만 하면 된다.
