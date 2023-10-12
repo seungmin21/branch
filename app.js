8327b6efe6dd3bc528baedaaf13878b65762c7cf
ohseungmin <ohmin2886@gmail.com>
2023-10-12 10:42:58 +0900

    기본 서버 제작

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+
+});
+
+server.listen(3500, (err) => {
+  if(err) {
+    console.error("서버 재구동 필요")
+  } else {
+    // 팁 콘솔에 링크를 만들어두면 링크를 클릭하기만 하면 된다.
+    console.log("3500 포트에 돌아감 http://localhost:3500");
+  }
+});
